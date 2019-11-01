ab48943acd4eca6521ef8429824feaff5a070f9e
weixingsun <weixing.sun@gmail.com>
2019-11-01 20:52:39 +0800

    add perf cause screenshot

--- test/python/main.py
+++ test/python/main.py
@@ -1,5 +1,6 @@
 import timeit, functools
 import fib  #cython
+import sys
 
 def fibonacci_py(n):
     a,b = 0,1
@@ -7,12 +8,29 @@ def fibonacci_py(n):
         a,b = b,a+b
     return b
 
-NUM=500000
+NUM=1000000
 TIMES=1
-tp  = timeit.Timer( functools.partial(fibonacci_py, NUM) ). timeit(TIMES)
-tc  = timeit.Timer( functools.partial(fib.fibonacci_cy, NUM) ). timeit(TIMES)
-tcs = timeit.Timer( functools.partial(fib.fibonacci_cy_styping, NUM) ). timeit(TIMES)
 
-print("Python: 1.000x : {0:.5f}".format(tp))
-print("Cython: {0:.3f}x : {1:.5f}".format(tp/tc, tc))
-print("Cython Static: {0:.3f}x : {1:.5f}".format(tp/tcs, tcs))
+def main():
+    if sys.argv[1] == "python":
+        tp  = timeit.Timer( functools.partial(fibonacci_py, NUM) ). timeit(TIMES)
+        print("Python: {0:.5f}".format(tp))
+    elif sys.argv[1] == "cython":
+        tc  = timeit.Timer( functools.partial(fib.fibonacci_cy, NUM) ). timeit(TIMES)
+        print("Cython: {0:.5f}".format(tc))
+    elif sys.argv[1] == "cython_typing":
+        tcs = timeit.Timer( functools.partial(fib.fibonacci_cy_styping, NUM) ). timeit(TIMES)
+        print("Cython: {0:.5f}".format(tcs))
+    elif sys.argv[1] == "all":
+        tp  = timeit.Timer( functools.partial(fibonacci_py, NUM) ). timeit(TIMES)
+        tc  = timeit.Timer( functools.partial(fib.fibonacci_cy, NUM) ). timeit(TIMES)
+        tcs = timeit.Timer( functools.partial(fib.fibonacci_cy_styping, NUM) ). timeit(TIMES)
+
+        print("Python: {0:.5f} seconds 1.000x".format(tp))
+        print("Cython: {1:.5f} seconds {0:.3f}x".format(tp/tc, tc))
+        print("Static: {1:.5f} seconds {0:.3f}x".format(tp/tcs, tcs))
+    else:
+        print("Invalid parameter: "+arg)
+
+if __name__ == "__main__":
+    main()
--- test/python/run.sh
+++ test/python/run.sh
@@ -1,8 +1,15 @@
 #cython3 --embed python1.py -o cython1.c
 #cc cython.c -o cython
 #gcc $(pkg-config --libs --cflags python3) cython1.c -o cython1
-python setup.py build_ext --inplace
+#python setup.py build_ext --inplace > build.log 2>&1 
 
-python main.py
+python main.py all
+#PERF_OPTS="cycles,instructions,cache-references,cache-misses"
+#perf stat -e $PERF_OPTS numactl --physcpubind=2 --localalloc python main.py python &
+#perf stat -e $PERF_OPTS numactl --physcpubind=2 --localalloc python main.py cython &
+#perf stat -e $PERF_OPTS numactl --physcpubind=2 --localalloc python main.py cython_typing &
 
-rm -rf build fib.c fib.cpython*.so
+#rm -rf build fib.c fib.cpython*.so
+
+#PID=`pgrep python`
+#perf record -F 99 -ag -p $PID -- sleep 20
