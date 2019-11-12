568e212d7ad1241f0da945693d73c18caa8a9c07
xuan wang <xuan@xuans-MacBook-Air.local>
2019-11-12 09:45:02 +0800

    add numba attempt

--- dl/mnist/main.py
+++ dl/mnist/main.py
@@ -1,18 +1,12 @@
-from main_cy import training as training_cy
-from main_py import training as training_py
-import timeit ,sys
-import functools as f
+import timeit as t
 
 TIMES=1
 #TIMES = int( sys.argv[1] )
-tc = timeit.Timer( f.partial(training_cy) ).timeit(TIMES)
-tp = timeit.Timer( f.partial(training_py) ).timeit(TIMES)
-print("Python: {0:.5f}".format(tp))
-print("Cython: {0:.5f}".format(tc))
-#import timeit
-
-#cy = timeit.timeit('main_cy.main()', setup='import main_cy', number=1)
-#py = timeit.timeit('main_py.main()', setup='import main_py', number=1)
-
-#print(cy, py)
-#print('cy is {}x faster'.format(py/cy))
+nb = t.timeit('nb.training()', setup='import main_nb as nb', number=TIMES)
+print("Numba:  {0:.5f}".format(nb))
+cy = t.timeit('cy.training()', setup='import main_cy as cy', number=TIMES)
+print("Cython: {0:.5f}".format(cy))
+py = t.timeit('py.training()', setup='import main_py as py', number=TIMES)
+print("Python: {0:.5f}".format(py))
+print("Cython: {0:.5f}".format(cy))
+print("Numba:  {0:.5f}".format(nb))
