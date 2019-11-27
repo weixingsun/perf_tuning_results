a92fe27ca387b79776f354c5020a42bec9313030
weixingsun <weixing.sun@gmail.com>
2019-11-27 19:42:45 +0800

    compile with gcc

--- bo/bios/run.sh
+++ bo/bios/run.sh
@@ -1,15 +1,18 @@
-#cython libjvm.pyx --embed
-#INC_PATH=/Users/xuan/anaconda3/include/python3.7m/
-#LIB_PATH=/Users/xuan/anaconda3/lib/python3.7/config-3.7m-darwin
+#!/bin/sh
 #gcc -shared libjvm.c -o libjvm.so -I$INC_PATH -L$LIB_PATH -lpython3.7m
 
-#gcc -c libjvm.c -o libjvm.o $(python3-config --cflags) 
-#gcc libjvm.o  -o libjvm.so $(python3-config --ldflags)
+do_cython() {
+  echo "cython"
+  cython --embed bios.py -o bios.c
+  gcc bios.c -o bios.exe $(python-config --cflags)  $(python-config --ldflags)
+  #gcc bios.c -o bios.exe $(python3-config --cflags) $(python3-config --ldflags)
+}
 
-rm -rf __pycache__/ bin build dist libbios.c libbios*.so main.spec
-python setup.py build_ext --inplace 
-pyinstaller --hidden-import hyperopt main.py --onefile
+do_pyinstaller() {
+  echo "pyinstaller"
+  rm -rf __pycache__/ bin build dist libbios.c libbios*.so main.spec
+  python setup.py build_ext --inplace 
+  pyinstaller --hidden-import hyperopt main.py --onefile
+}
 
-#INC_PATH=/usr/include/python3.7
-#PID=`pgrep python`
-#perf record -F 99 -ag -p $PID -- sleep 20
+do_cython
