697f228b9cac9d7f679fb46829ee37a65f2a97f2
weixingsun <weixing.sun@gmail.com>
2019-12-04 20:26:42 +0800

    add static for linux

--- bo/kernel/build.sh
+++ bo/kernel/build.sh
@@ -4,8 +4,18 @@
 #python2 -m pip install hyperopt
 do_cython() {
   echo "cython"
-  cython --embed Optim.py -o tuner.c
-  gcc tuner.c -o tuner.exe $(python-config --cflags)  $(python-config --ldflags)
+  #cython --embed Optim.py -o tuner.c
+  CMD="gcc tuner.c -o tuner.exe $(python-config --cflags)  $(python-config --ldflags) -lz "
+  OS=`uname`
+  if [[ $OS -eq "Darwin" ]]; then
+    #dynamic
+    STATIC=""
+    CMD2="$CMD"
+  else
+    CMD2=`echo "$CMD -static"|sed -e "s/dynamic/static/g"`
+  fi
   #gcc tuner.c -o tuner.exe $(python3-config --cflags) $(python3-config --ldflags)
+  echo $CMD2
+  $CMD2
 }
 do_cython
