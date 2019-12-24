835068fba8a7d30555d797935c97ac681c451bc0
weixingsun <weixing.sun@gmail.com>
2019-12-24 15:21:58 +0800

    single host

--- test/nginx2/run_svr.sh
+++ test/nginx2/run_svr.sh
@@ -1,28 +1,61 @@
+#!/bin/bash
+
 setup(){
-  insmod kmod/rte_kni.ko carrier=on
-  insmod kmod/igb_uio.ko
-  ifconfig ens1f1 down
-  dpdk/usertools/dpdk-devbind.py --bind=igb_uio 0000:06:00.1
-  rm -rf /usr/local/nginx_fstack/logs/error.log
-  grep Huge /proc/meminfo
+  export RTE_ANS=/s1/dpdk-ans
+  DPDK_DIR=$RTE_ANS/dpdk-18.11
+  export RTE_SDK=$DPDK_DIR
+  export RTE_TARGET=x86_64-native-linuxapp-gcc
+  make config T=x86_64-native-linuxapp-gcc
+  make install T=x86_64-native-linuxapp-gcc DESTDIR=x86_64-native-linuxapp-gcc
+  KMOD_DIR=$DPDK_DIR/x86_64-native-linuxapp-gcc/kmod/
+  insmod $KMOD_DIR/igb_uio.ko
+  insmod $KMOD_DIR/rte_kni.ko
+  TOOLS_DIR=$DPDK_DIR/usertools/
+  #python $DPDK_DIR/usertools/dpdk-devbind.py --status
+  #ETH=`python $DPDK_DIR/usertools/dpdk-devbind.py --status |grep 10-Gigabit|grep -v Active|awk -F"if=" '{print $2}'|awk '{print $1}'`
+  cd $RTE_ANS/ans
+  # ans_main.h    #define MAX_TX_BURST 1
+  make
+  ########### /usr/local/nginx
+  cd $RTE_ANS/dpdk-nginx
+  ./configure  --with-http_dav_module
+  make && make install
 }
 
-nginx(){
-  /usr/local/nginx_fstack/sbin/nginx -s stop
+svc(){
+  IP=$1  #10.0.0.2
+  #pkill ans
+  #--enable-kni --enable-ipsync  --enable-jumbo --max-pkt-len 9001
+  #-w = --pci-whitelist
+  #-l = --lcore
+  #-n #memory channels
+  #--
+  #-p #port mask
+  #--config=(port,queue,lcore)
+  MEM="--base-virtaddr=0x2aaa2aa0000"
+  PCI="-w 0000:58:00.0 "  #jbb3
+  PCI="-w 0000:06:00.0 "  #dr1.wrk
+  PCI="-w 0000:06:00.1 "  #dr1.nginx
+  CPU1="-l 1 -n 4 -- -p 0x1 --config='(0,0,1)'"
+  #CPU2="-l 2 -n 4 -- -p 0x1 --config='(0,0,2)'"
+  #CPU1_2="-l 1,2 -n 4 -- -p 0x1 --config='(0,0,3)'"
+  CPU="$CPU1"
+  echo "starting ans.nginx"
+  #PREFIX="--file-prefix=nginx"
+  #echo "nohup ans/build/ans $PREFIX $PCI $MEM $CPU > ans.nginx.log 2>&1 &"
+  #      nohup ans/build/ans $PREFIX $PCI $MEM $CPU > ans.nginx.log 2>&1 &
+  nohup ans/build/ans $PREFIX $PCI $CPU > ans.nginx.log 2>&1 &
+  echo "starting 10s"
+  sleep 10
+  cli/build/anscli $PREFIX "ip addr add $IP/24 dev veth0"
+  cli/build/anscli $PREFIX "ip addr show"
+  sleep 1
+  /usr/local/nginx/sbin/nginx
   sleep 5
-  /usr/local/nginx_fstack/sbin/nginx
-  sleep 20
 }
-
-ip(){
-  ipaddr=`grep "^addr" config.ini |awk -F= '{print $2}'`
-  netmask=`grep "^netmask" config.ini |awk -F= '{print $2}'`
-  broadcast=`grep "^broadcast" config.ini |awk -F= '{print $2}'`
-  gateway=`grep "^gateway" config.ini |awk -F= '{print $2}'`
-  macaddr="90:e2:ba:c5:dc:bc"
-  ifconfig veth0 $ipaddr  netmask $netmask  broadcast $broadcast hw ether $macaddr
-  route add -net 0.0.0.0 gw $gateway dev veth0
-  echo 1 > /sys/class/net/veth0/carrier # if `carrier=on` not set while `insmod rte_kni.ko`
-  # route add -net ...  # other route rules
-}
-nginx
+##
+# ans/build/ans -w 0000:06:00.1 -l 1 -n 4 -- -p 0x1 --config='(0,0,1)'
+# ans/build/ans --file-prefix=wrks -w 0000:06:00.0 --base-virtaddr=0x2aaa2aa0000 -l 2 -n 4 -- -p 0x1 --config='(0,0,2)'
+##
+svc 10.10.10.2
+SVR_IP="10.10.10.2"
--- test/nginx2/run_wrk.sh
+++ test/nginx2/run_wrk.sh
@@ -10,17 +10,17 @@ setup(){
   KMOD_DIR=$DPDK_DIR/x86_64-native-linuxapp-gcc/kmod/
   insmod $KMOD_DIR/igb_uio.ko
   insmod $KMOD_DIR/rte_kni.ko
-  TOOLS_DIR=$BASE_DIR/usertools/
+  TOOLS_DIR=$DPDK_DIR/usertools/
   #python $DPDK_DIR/usertools/dpdk-devbind.py --status
   #ETH=`python $DPDK_DIR/usertools/dpdk-devbind.py --status |grep 10-Gigabit|grep -v Active|awk -F"if=" '{print $2}'|awk '{print $1}'`
-  cd $BASE_DIR/ans
+  cd $RTE_ANS/ans
   # ans_main.h    #define MAX_TX_BURST 1
   make
 }
 
 svc(){
-  IP=$1
-  pkill ans
+  IP=$1  #10.0.0.2
+  #pkill ans
   #--enable-kni --enable-ipsync  --enable-jumbo --max-pkt-len 9001
   #-w = --pci-whitelist
   #-l = --lcore
@@ -29,18 +29,20 @@ svc(){
   #-p #port mask
   #--config=(port,queue,lcore)
   MEM="--base-virtaddr=0x2aaa2aa0000"
-  PCI="-w 0000:58:00.0 "
-  CPU1="-l 1 -n 4  -- -p 0x1 --config='(0,0,1)'"
+  PCI="-w 0000:58:00.0 "  #jbb3
+  PCI="-w 0000:06:00.0 "  #dr1
+  CPU1="-l 1 -n 4 -- -p 0x1 --config='(0,0,1)'"
+  CPU2="-l 2 -n 4 -- -p 0x1 --config='(0,0,2)'"
   CPU1_2="-l 1,2 -n 4 -- -p 0x1 --config='(0,0,3)'"
-  CPU="$CPU1"
+  CPU="$CPU2"
   echo "sleeping 10s"
   sleep 10
-  echo "nohup ans/build/ans $PCI $MEM $CPU > ans.log 2>&1 &"
-  nohup ans/build/ans $PCI $MEM $CPU > ans.log 2>&1 &
+  echo "nohup ans/build/ans --file-prefix=wrks $PCI $MEM $CPU > ans.wrk.log 2>&1 &"
+  nohup ans/build/ans --file-prefix=wrks $PCI $MEM $CPU > ans.wrk.log 2>&1 &
   echo "starting 10s"
   sleep 10
-  cli/build/anscli "ip addr add $IP/24 dev veth0"
-  sleep 10
+  #cli/build/anscli --file-prefix=wrks "ip addr add $IP/24 dev veth0"
+  #sleep 10
 }
 
 mon(){
@@ -69,6 +71,30 @@ mon(){
   CPUs=`tail -n1 $CPU_SVR|awk '{printf("%d+%d",$3,$5)}'`
   echo "$T	$C	$RPSa	$RPSx	$LAT50	$LAT75	$LAT90	$LAT99	$LATx	$CPUc	$CPUs"
 }
+mon1(){
+  INT="2 9"
+  CPU="1-2"
+  CLT=$1
+  T=$2
+  C=$3
+  LOG_CPU=logs/mpstat.client_$T_$C.log
+  mpstat -P $CPU $INT > $LOG_CPU 2>&1 &
+  RPSa=`tail -f $CLT | grep -m 1 'Req/Sec'|awk '{print $2}'`
+  RPSx=`grep 'Req/Sec' $CLT|awk '{print $4}'`
+  LAT50=`grep ' 50%' $CLT |awk '{print $2}'`
+  LAT75=`grep ' 75%' $CLT |awk '{print $2}'`
+  LAT90=`grep ' 90%' $CLT |awk '{print $2}'`
+  LAT99=`grep ' 99%' $CLT |awk '{print $2}'`
+  LAT50=${LAT50/.00/}
+  LAT75=${LAT75/.00/}
+  LAT90=${LAT90/.00/}
+  LAT99=${LAT99/.00/}
+  LATx=`grep Latency $CLT|grep -v Dist |awk '{print $4}'`
+  LATx=${LATx/.00/}
+  CPUs=`tail -n1 $LOG_CPU|awk '{printf("%d+%d",$3,$5)}'`
+  CPUc=`tail -n2 $LOG_CPU|head -1|awk '{printf("%d+%d",$3,$5)}'`
+  echo "$T      $C      $RPSa   $RPSx   $LAT50  $LAT75  $LAT90  $LAT99  $LATx   $CPUc   $CPUs"
+}
 wrk(){
   IP=$1
   THREADS=$2
@@ -76,7 +102,7 @@ wrk(){
   TIME=20s
   #RPS=$5
   URL="http://$IP/index.html"
-  WRK="dpdk-httpperf/dpdk-httpperf"
+  WRK="dpdk-httpperf/dpdk-httpperf --file-prefix=wrks"
   echo "$WRK --latency -t$THREADS -c$CONN -d$TIME $URL"
   $WRK --latency -t$THREADS -c$CONN -d$TIME $URL
 
@@ -85,8 +111,8 @@ mkdir -p logs
 rm -rf logs/*
 #svc 10.20.10.20
 echo "PROC	CONN	RPSavg	RPSmax	LAT50	LAT75	LAT90	LAT99  	LATmax	CPU_c	CPU_s"
-SVR_IP="10.20.10.10"
-R1="1 2 4 8"
+SVR_IP="10.10.10.2"
+R1="1 2 4"
 R2="256 128 64 32 16 8 4 2 1"
 for ts in $R1; do
   for cs in $R2; do
@@ -94,8 +120,8 @@ for ts in $R1; do
       LOG="logs/client$ts.$cs.log"
       #echo "T=$ts, C=$cs, CLIENT=$LOG"
       wrk $SVR_IP $ts $cs > $LOG 2>&1 &
-      mon $LOG $ts $cs
-      sleep 5
+      mon1 $LOG $ts $cs
+      sleep 10
     fi
   done
   sleep 10
